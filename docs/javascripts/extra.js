ebad1bd55795a0812902e36b81ab95df3d85049f
Soyoon-Choi <sy.choi@altibase.com>
2024-11-21 10:39:25 +0900

    7.1 eng Nok 싱크

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,12 +1,12 @@
 const floatingMenuButton = document.getElementById("floatingMenuButton");
 const sideMenu = document.getElementById("sideMenu");
 
-// 햄버거 버튼 클릭 시 메뉴 보이기/숨기기
+// 매뉴얼 확장 버튼 클릭 시 매뉴얼 리스트 보이기/숨기기
 floatingMenuButton.addEventListener("click", () => {
 sideMenu.classList.toggle("open");
 });
 
-// 메뉴를 클릭하거나 메뉴 밖을 클릭 시 메뉴 닫기
+// 매뉴얼 확장 버튼 클릭하거나 버튼 밖을 클릭 시 매뉴얼 리스트 닫기
 document.addEventListener("click", (e) => {
 if (!sideMenu.contains(e.target) && !floatingMenuButton.contains(e.target)) {
     sideMenu.classList.remove("open");
